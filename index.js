2b41132b1b5bccc69dcb9c75f5815d5d4d27455b
NjeriMarion <njerimarion@gmail.comgit config --global user.email njerimarion@gmail.comhistorygit clone https://github.com/NjeriMarion/tools_new_2.git>
2023-05-29 16:47:40 +0300

    media querries to make the website responsive

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('abou').style.color = 'orange'
+document.getElementById('abou').style.borderBottom = 'double 5px'
+document.getElementById('abou').style.paddingLeft= '20%'
+document.getElementById('abou').style.marginBottom = '25px'
+document.getElementById('h2').style.fontSize= '30px'
+document.getElementById('h3').style.fontSize= '30px'
+document.getElementById('h2').style.fontWeight= '900'
